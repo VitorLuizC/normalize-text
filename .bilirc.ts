2a3ae1fe76731e3cdfcd5c51062b29ade3d36c73
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2020-05-05 14:05:36 -0300

    :arrow_up: upgrade devDependencies

--- .bilirc.ts
+++ .bilirc.ts
@@ -5,18 +5,18 @@ const config: Config = {
   input: 'src/index.ts',
   output: {
     format: ['es', 'cjs', 'umd', 'umd-min'],
-    moduleName: 'normalizeText'
+    moduleName: 'normalizeText',
   },
   babel: {
-    minimal: true
+    minimal: true,
   },
   plugins: {
     typescript2: {
       clean: true,
       tsconfig: 'tsconfig.build.json',
-      useTsconfigDeclarationDir: true
-    }
-  }
+      useTsconfigDeclarationDir: true,
+    },
+  },
 };
 
 export default config;
