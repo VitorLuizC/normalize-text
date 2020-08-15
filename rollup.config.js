a96ed166c6fb66358392111c793a51f6671a0c80
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2020-08-14 23:19:13 -0300

    🏗 use Rollup.js itself instead of bili abstraction

---
+++ rollup.config.js
@@ -0,0 +1,57 @@
+// @ts-check
+
+import { terser } from 'rollup-plugin-terser';
+import resolve from '@rollup/plugin-node-resolve';
+
+/**
+ * Options of Rollup.js.
+ * @type {Array.<import('rollup').RollupOptions>}
+ */
+const options = [
+  {
+    input: './src/index.js',
+    external: ['@bitty/pipe'],
+    output: [
+      getOutputOption({
+        format: 'commonjs',
+        file: './dist/index.js',
+      }),
+      getOutputOption({
+        format: 'esm',
+        file: './dist/index.esm.js',
+      }),
+    ],
+  },
+  {
+    input: './src/index.js',
+    plugins: [resolve()],
+    output: [
+      getOutputOption({
+        format: 'umd',
+        name: 'normalizeText',
+        file: './dist/index.umd.js',
+      }),
+      getOutputOption({
+        format: 'umd',
+        name: 'normalizeText',
+        file: './dist/index.umd.min.js',
+        plugins: [terser()],
+      }),
+    ],
+  },
+];
+
+/**
+ * Creates an output option.
+ * @param {import('rollup').OutputOptions} options
+ * @returns {import('rollup').OutputOptions}
+ */
+function getOutputOption(options) {
+  return {
+    ...options,
+    exports: 'named',
+    sourcemap: true,
+  };
+}
+
+export default options;
